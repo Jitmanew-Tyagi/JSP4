2c886bb0cca8f9613c9b748a3bc6df9c36684308
jitmanew-tyagi <jitmanew@gmail.com>
2022-02-11 13:26:23 +0530

    DP Class 2

--- Dp.java
+++ Dp.java
@@ -6,7 +6,7 @@ import java.util.*;
 public class Dp{
 
  // 1. Faith
- // 2. Recursive Tree --> ArrayList Vala 
+ // 2. Recursive Tree
  // 3. Recursive Code
  // 4. Memoization
  // 5. Observation 
@@ -62,6 +62,8 @@ public class Dp{
         }
         return ans;
     }
+
+
     public static int mazePathMemo(int sr, int sc, int er, int ec, int[][] dp) {
         if(sr == er && sc == ec) return dp[sr][sc] = 1;
         if(dp[sr][sc] != 0) return dp[sr][sc];
@@ -85,7 +87,7 @@ public class Dp{
                     dp[sr][sc] = 1;
                     continue;
                 }
-                
+
                 // if(dp[sr][sc] != 0) return dp[sr][sc];
                 int[][] dirn = {{0, 1}, {1, 0}, {1, 1}};
                 int ans = 0;
@@ -102,13 +104,81 @@ public class Dp{
         return dp[SR][SC];
     }
 
+
+    public int memo(String s, HashMap<String, Integer> dp) {
+            if(s.length() == 0) {
+                dp.put(s, 1);
+                return 1;
+            }
+            if(dp.containsKey(s)) return dp.get(s);
+            int n = s.charAt(0) - '0';
+            if(n == 0) {
+                dp.put(s, 0);
+                return 0;
+            }
+            int ans = 0;
+            ans += memo(s.substring(1), dp);
+            if(s.length() > 1) {
+                n = (n * 10) + (s.charAt(1) - '0');
+                if(n <= 26) {
+                    ans += memo(s.substring(2), dp);
+                }
+            }
+            dp.put(s, ans);
+            return ans;
+        }
+        
+        public int arrRec(String s, int idx, int[] dp) {
+            if(idx == s.length()) return dp[idx] = 1;
+            int n = s.charAt(idx) - '0';
+            if(dp[idx] != -1) return dp[idx];
+            if(n == 0) return dp[idx] = 0;
+            int ans = 0;
+            ans += arrRec(s, idx + 1, dp);
+            if(idx < s.length() - 1) {
+                n = n * 10 + (s.charAt(idx + 1) - '0');
+                if(n <= 26){
+                    ans += arrRec(s, idx + 2, dp);
+                }
+            }
+            return dp[idx] = ans;
+        } 
+        
+         public int arrRecTab(String s, int IDX, int[] dp) {
+            //  YNeos
+            for(int idx = s.length(); idx >= IDX; idx --) {
+                if(idx == s.length()){
+                    dp[idx] = 1;
+                    continue;
+                }
+                int n = s.charAt(idx) - '0';
+                // if(dp[idx] != -1) return dp[idx];
+                if(n == 0) {
+                    dp[idx] = 0;
+                    continue;
+                }
+                int ans = 0;
+                ans += dp[idx + 1];// arrRec(s, idx + 1, dp);
+                if(idx < s.length() - 1) {
+                   n = n * 10 + (s.charAt(idx + 1) - '0');
+                   if(n <= 26){
+                       ans += dp[idx + 2];//arrRec(s, idx + 2, dp);
+                   }
+                }
+                dp[idx] = ans;
+            }
+            return dp[IDX];
+        }
+
     public static void main(String[] args) throws Exception {
         // write your code here
-        int[][] dp = new int[7][7];
-        // Arrays.fill(dp, -1);
+        int[] dp = new int[9];
+        Arrays.fill(dp, -1);
         // System.out.println(fiboMem(7, dp));
         // System.out.println(fiboTab(7, dp));
-        System.out.println(mazePathTab(0, 0, 6, 6, dp));
-        display2D(dp);
+        System.out.println(arrRec("11234451", 0, dp));
+        display(dp);
     }
+
+
 }
